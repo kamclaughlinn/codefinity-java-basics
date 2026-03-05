c0611cf66f1920788b420d4e464e92d0f5062dc8
kamclaughlinn <>
2026-03-05 14:47:50 +0000

    Array Iteration

---
+++ arrays/array_iteration/Main.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Main {
+    public static double calculateAverage(int[] workHours) {
+        int sizeMassive = workHours.length;
+        int totalHours = 0;
+
+        for (int i = 0; i < sizeMassive; i++) {
+            totalHours += workHours[i];
+        }
+
+         double avgHours = totalHours / (double) sizeMassive;
+
+        return avgHours;
+    }
+    
+    public static void main(String[] args) {
+        int[] workHours = {7, 8, 6, 9, 7, 7, 8, 7, 6, 9};
+        double average = calculateAverage(workHours);
+
+        if (average < 8) {
+            System.out.println("Penalty: employee worked " + average + " hours on average.");
+        } else {
+            System.out.println("Good job! Employee worked " + average + " on average.");
+        }
+    }
+}
